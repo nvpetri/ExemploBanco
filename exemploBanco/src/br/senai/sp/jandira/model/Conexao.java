0f201aa64e67054c7fa28ef55a341b9ec98afa3c
NPS <nicolaspetri18@gmail.com>
2023-11-08 11:33:12 -0300

    Iniciando

--- exemploBanco/src/br/senai/sp/jandira/model/Conexao.java
+++ exemploBanco/src/br/senai/sp/jandira/model/Conexao.java
@@ -2,7 +2,17 @@ package br.senai.sp.jandira.model;
 
 public class Conexao {
 
+    private String servidor, senha, user, banco;
+    private int porta;
     public Conexao(){
+        this.servidor = "localhost";
+        this.banco = "db_empresa";
+        this.user = "root";
+        this.senha = "bcd127";
+        this.porta = 3306;
+    }
+
+    public boolean connectDrive(){
         
     }
 }
